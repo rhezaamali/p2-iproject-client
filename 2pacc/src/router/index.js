2bf17779d5e811bc10a79f78d50a0bcb22aeb5f2
rhezaamali <rhezaamali@gmail.com>
2022-02-24 13:00:32 +0700

    deploy

--- 2pacc/src/router/index.js
+++ 2pacc/src/router/index.js
@@ -1,5 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Register from "../views/Register.vue";
+import Login from "../views/Login.vue";
+import Main from "../views/Main.vue";
+
+import MemeDb from "../components/MemeDb.vue";
+import MemeApi from "../components/MemeApi.vue";
+import AddMeme from "../components/AddMeme.vue";
+import Likes from "../views/Likes.vue";
+import Jokes from "../views/Jokes.vue";
 
 Vue.use(VueRouter);
 
@@ -7,41 +16,41 @@ const routes = [
   {
     path: "/login",
     name: "Login",
-    component: () => import("../views/Login.vue"),
+    component: Login,
   },
   {
     path: "/register",
     name: "Register",
-    component: () => import("../views/Register.vue"),
+    component: Register,
   },
   {
     path: "/",
     name: "Main",
-    component: () => import("../views/Main.vue"),
+    component: Main,
     children: [
       {
         path: "/",
-        component: () => import("../components/MemeDb.vue"),
+        component: MemeDb,
       },
       {
         path: "shuffle",
-        component: () => import("../components/MemeApi.vue"),
+        component: MemeApi,
       },
       {
         path: "addmeme",
-        component: () => import("../components/AddMeme.vue"),
+        component: AddMeme,
       },
     ],
   },
   {
     path: "/likes",
     name: "Likes",
-    component: () => import("../views/Likes.vue"),
+    component: Likes,
   },
   {
     path: "/jokes",
     name: "Jokes",
-    component: () => import("../views/Jokes.vue"),
+    component: Jokes,
   },
 ];
 
