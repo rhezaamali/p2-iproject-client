9f1cd1dbade964b2ca95fa5fbe42c29350b75159
rhezaamali <rhezaamali@gmail.com>
2022-02-24 14:54:31 +0700

    deploy

--- 2pacc/src/api/axios.js
+++ 2pacc/src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "http://localhost:3000",
-});
-
 // const instance = axios.create({
-//   baseURL: "https://two-pac.herokuapp.com/",
+//   baseURL: "http://localhost:3000",
 // });
+
+const instance = axios.create({
+  baseURL: "https://two-pac.herokuapp.com/",
+});
 
 export default instance;
