c14e1f6be3633848961154a8e42ebc7253f72fc2
rhezaamali <rhezaamali@gmail.com>
2022-02-24 12:49:07 +0700

    deploy

--- 2pacc/src/router/index.js
+++ 2pacc/src/router/index.js
@@ -51,16 +51,16 @@ const router = new VueRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
-  if (to.name == "Register" && !localStorage.access_token) {
-    next();
-  } else if (to.name !== "Login" && !localStorage.access_token) {
-    next({ name: "Login" });
-  } else if (to.name === "Login" && localStorage.access_token) {
-    next("/");
-  } else {
-    next();
-  }
-});
+// router.beforeEach((to, from, next) => {
+//   if (to.name == "Register" && !localStorage.access_token) {
+//     next();
+//   } else if (to.name !== "Login" && !localStorage.access_token) {
+//     next({ name: "Login" });
+//   } else if (to.name === "Login" && localStorage.access_token) {
+//     next("/");
+//   } else {
+//     next();
+//   }
+// });
 
 export default router;
